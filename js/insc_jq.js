cc958c9383e4e168238d096bdf6c77a783828a33
nitrox60 <bouldog1511@hotmail.fr>
2012-12-07 19:16:00 +0100

    ajout sur insc_jq
    Correction bug

--- js/insc_jq.js
+++ js/insc_jq.js
@@ -1,41 +1,83 @@
 ﻿$(function(){
 
+	var nom=false;
 	$("#nom").blur(function(){
 	
 		if($(this).val().length>=20)
 		{
-			$("#nom").after("<span class=\"error\"> Le nom doit être inférieur à 20 caractères</span>");
+			if(!nom)
+			{
+				$(this).after("<span class=\"error\"> Le nom doit être inférieur à 20 caractères</span>");
+				nom=true;
+			}
 	
 		}
-	
+		else
+		{
+			$("#nom+ .error").hide();
+			nom=false;
+		}
 	});
+	
+	
+	var prenom=false;
 	
 	$("#prenom").blur(function(){
 	
 		if($(this).val().length>=20)
 		{
-			$("#prenom").after("<span class=\"error\"> Le nom doit être inférieur à 20 caractères</span>");
+			if(!prenom)
+			{
+				$(this).after("<span class=\"error\"> Le nom doit être inférieur à 20 caractères</span>");
+				prenom=true;
+			}
 			
+		}
+		else
+		{
+			$("#prenom + .error").hide();
+			prenom=false;
 		}
 	
 	});
 	
+	var rue=false;
 	$("#rue").blur(function(){
 	
 		if($(this).val().length>=50)
 		{
-			$(this).after("<span class=\"error\"> La rue doit être inférieur à 50 caractères</span>");
+			if(!rue)
+			{
 			
+				$(this).after("<span class=\"error\"> La rue doit être inférieur à 50 caractères</span>");
+				rue=true;
+			}
 		}
-	
+		else 
+		{
+			$("#rue + .error").hide();
+			rue=true;
+		}
 	});
 	
+	var cp=false;
 	$("#cp").blur(function(){
 	
 		
 		
-		if(!(/^[0-9]{5}$/.test($(this).val()))) 
-			$(this).after("<span class=\"error\"> Code postal incorrect format XXXXX</span>");
+		if ( (!(/^[0-9]{5}$/.test($(this).val()))) || (/^(0){2}[0-9]{3}$/.test($(this).val()=="00000")))
+		{
+			if(!cp)
+			{
+				$(this).after("<span class=\"error\"> Code postal incorrect format XXXXX</span>");
+					cp=true;
+			}
+		}
+		else 
+		{	
+			$("#cp + .error").hide();
+			cp=false;
+		}
 		
 	});
 
